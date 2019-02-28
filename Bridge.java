8939efaf7b99d51e54785be64f8b8408b50ae968
spencerjolie <44411669+spencerjolie@users.noreply.github.com>
2019-02-28 14:26:28 -0500

    Create Bridge.java

---
+++ Bridge.java
@@ -0,0 +1,7 @@
+public interface Bridge<E>
+{
+   public void bridgeLength(E len);
+   public void bridgeHeight(E heigth);
+   public void weightLimit(E wl);
+   public void materials(String[] mat);
+}
