b7f1554100dd0bbb427aa1e377f3af33bc3ac20e
eddielife2k <44607352+eddielife2k@users.noreply.github.com>
2019-02-28 14:50:14 -0500

    Update CarBridge.java

--- CarBridge.java
+++ CarBridge.java
@@ -1,4 +1,4 @@
-public abstract class CarBridge<E> implements Bridge
+public abstract class CarBridge<E> implements Bridge<E>
 {
     protected E length;
     protected String[] materials;
@@ -6,43 +6,58 @@ public abstract class CarBridge<E> implements Bridge
     protected E weightLimit;
     protected int numLanes;
     protected E speedLimit;
-    public CarBridge() {
+    public CarBridge(E len, String[] mat, E h, E wl, int nL, E sL) {
+        length = len;
+        mat = materials;
+        h = height;
+        wl = weightLimit;
+        nL = numLanes;
+        sL = speedLimit;
     }
     public void bridgeLength(E len) {
+        len = length;
     }
     public E getBridgeLength() {
         return length;
     }
     public void materials(String[] mat) {
+        mat = materials;
     }
      public String getMaterials() {
         String temp = "";
         for(String str: materials) {
-            temp += str; 
+            temp += str + ", "; 
         }
         return temp;
     }
-    public void bridgeHeight(E height) {
+    public void bridgeHeight(E h) {
+        h = height;
     }
      public E getBridgeHeight() {
         return height;
     }
     public void weightLimit(E wl) {
+        weightLimit = wl;
     }
     public E getWeightLimit() {
         return weightLimit;
     }
     public void numLanes(int nL) {
+        numLanes = nL;
     }
     public int getNumLanes() {
         return numLanes;
     }
     public void speedLimit(E lim) {
+        lim = speedLimit;
     }
     public E getSpeedLimit() {
         return speedLimit;
     }
     public abstract void tensions(double[] ten);
-    public abstract void cantelieverLength(E len);
+    public abstract String getTensions();
+    public abstract void cantileverLength(E len);
+    public abstract E getCantileverLength();
     public abstract void arcRad(E rad);
+    public abstract E getArcRad();
 }
